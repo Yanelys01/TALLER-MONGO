3b980d24564fd11800ae2428488b5368ac971227
Yanelys01 <91100469+Yanelys01@users.noreply.github.com>
2021-11-15 23:42:23 -0500

    Add files via upload

---
+++ role.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+import role from "../controllers/role.js";
+
+const router = express.Router();
+
+router.post("/registerRole", role.registerRole);
+
+export default router;
